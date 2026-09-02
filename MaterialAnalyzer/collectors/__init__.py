619f907a3d6b9091cf0ad43230c0e58da2db82c3
sooyeonkim96 <80865733+SooyeonKim-github@users.noreply.github.com>
2026-09-02 20:40:21 +0900

    Add MaterialAnalyzer collectors package

---
+++ MaterialAnalyzer/collectors/__init__.py
@@ -0,0 +1,5 @@
+from .naver_news import NaverNewsCollector
+from .opendart import OpenDartCollector
+from .policy_briefing import PolicyBriefingCollector
+
+__all__ = ["NaverNewsCollector", "OpenDartCollector", "PolicyBriefingCollector"]
