0efe5b9f54240c5bbf710f57806eb623a1b494ba
sooyeonkim96 <80865733+SooyeonKim-github@users.noreply.github.com>
2026-09-04 22:34:57 +0900

    Export EventRepository

--- MaterialAnalyzer/news/storage/__init__.py
+++ MaterialAnalyzer/news/storage/__init__.py
@@ -1,6 +1,13 @@
 from .article_repository import ArticleRepository
 from .cluster_repository import ClusterRepository
 from .database import Database
+from .event_repository import EventRepository
 from .source_state_repository import SourceStateRepository
 
-__all__ = ["Database", "ArticleRepository", "SourceStateRepository", "ClusterRepository"]
+__all__ = [
+    "Database",
+    "ArticleRepository",
+    "SourceStateRepository",
+    "ClusterRepository",
+    "EventRepository",
+]
