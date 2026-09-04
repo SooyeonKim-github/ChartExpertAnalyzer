3a43694fcc554989f89ff4fb39b73c4185195da2
sooyeonkim96 <80865733+SooyeonKim-github@users.noreply.github.com>
2026-09-04 15:55:55 +0900

    feat: add performance package

---
+++ LeaderStockAnalyzer/leader_stock_analyzer/performance/__init__.py
@@ -0,0 +1,4 @@
+from .attribution import PerformanceAttributionEngine
+from .forward_performance import ForwardPerformanceEngine
+
+__all__ = ["ForwardPerformanceEngine", "PerformanceAttributionEngine"]
