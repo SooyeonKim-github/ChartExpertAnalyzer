330927cf1596bbea82eb152ca6316b4e58cfcd98
sooyeonkim96 <80865733+SooyeonKim-github@users.noreply.github.com>
2026-09-05 09:24:18 +0900

    Export NoveltyRepository

--- MaterialAnalyzer/news/storage/__init__.py
+++ MaterialAnalyzer/news/storage/__init__.py
@@ -2,6 +2,7 @@ from .article_repository import ArticleRepository
 from .cluster_repository import ClusterRepository
 from .database import Database
 from .event_repository import EventRepository
+from .novelty_repository import NoveltyRepository
 from .source_state_repository import SourceStateRepository
 
 __all__ = [
@@ -10,4 +11,5 @@ __all__ = [
     "SourceStateRepository",
     "ClusterRepository",
     "EventRepository",
+    "NoveltyRepository",
 ]
