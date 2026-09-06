63824a168449d10d52ec5c858cc6c97a98d26ec0
sooyeonkim96 <80865733+SooyeonKim-github@users.noreply.github.com>
2026-09-06 12:43:14 +0900

    Export MaterialScoreRepository

--- MaterialAnalyzer/news/storage/__init__.py
+++ MaterialAnalyzer/news/storage/__init__.py
@@ -2,6 +2,7 @@ from .article_repository import ArticleRepository
 from .cluster_repository import ClusterRepository
 from .database import Database
 from .event_repository import EventRepository
+from .material_score_repository import MaterialScoreRepository
 from .novelty_repository import NoveltyRepository
 from .source_state_repository import SourceStateRepository
 
@@ -12,4 +13,5 @@ __all__ = [
     "ClusterRepository",
     "EventRepository",
     "NoveltyRepository",
+    "MaterialScoreRepository",
 ]
