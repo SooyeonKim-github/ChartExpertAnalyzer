8effc44ab3f9d7ca82d61d5dbbd374e8cbae4d16
sooyeonkim96 <80865733+SooyeonKim-github@users.noreply.github.com>
2026-09-06 16:32:28 +0900

    feat(leader): export lifecycle engine

--- LeaderStockAnalyzer/leader_stock_analyzer/__init__.py
+++ LeaderStockAnalyzer/leader_stock_analyzer/__init__.py
@@ -1,6 +1,13 @@
 from .analyzer import LeaderStockAnalyzer
 from .config import load_config
+from .lifecycle import LeaderLifecycleEngine
 from .models import LeaderResult
 from .screen import screen_date
 
-__all__ = ["LeaderStockAnalyzer", "LeaderResult", "load_config", "screen_date"]
+__all__ = [
+    "LeaderStockAnalyzer",
+    "LeaderLifecycleEngine",
+    "LeaderResult",
+    "load_config",
+    "screen_date",
+]
