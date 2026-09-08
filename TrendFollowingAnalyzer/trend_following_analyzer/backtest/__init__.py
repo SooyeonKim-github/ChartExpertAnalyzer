fb2bbfa50ceee5a9b35332aad436649a418469f8
sooyeonkim96 <80865733+SooyeonKim-github@users.noreply.github.com>
2026-09-08 21:32:47 +0900

    feat(trend-following): add range backtest package

---
+++ TrendFollowingAnalyzer/trend_following_analyzer/backtest/__init__.py
@@ -0,0 +1,3 @@
+from .range_v1 import RangeRunMeta, parse_date_range, run_range_v1, summarize_strategy_buckets
+
+__all__ = ["RangeRunMeta", "parse_date_range", "run_range_v1", "summarize_strategy_buckets"]
